ac347d4ef768b38f13addf05516915fdedde1244
brandon2997 <bscruzguevara@email.neit.edu>
2020-10-29 11:04:14 -0400

    end of week 4

--- l/W4lab/JS/Script.js
+++ l/W4lab/JS/Script.js
@@ -2,9 +2,19 @@
 
 var C = document.querySelector('canvas');
 var ctx = C.getContext('2d')
+var mario= new Image()
+mario.src = 'images/mairo.png'
+var timer = requestAnimationFrame(draw)
+var x = 0
 
 function draw(){
-    ctx.fillStyle = 'red'
+    timer = requestAnimationFrame(draw)
+    ctx.clearRect(0,0, C.width, C.height)
+   x++
+   if(x > console.width){
+       x = -200
+   }
+    /* ctx.fillStyle = 'red'
     ctx.fillRect(200,100,200,200);
     // draw a line
     ctx.moveTo(0,0);
@@ -15,9 +25,19 @@ function draw(){
     ctx.lineTo(0,600);
     ctx.stroke();
     //draw a circle
+    ctx.lineWidth =5
     ctx.beginPath()
-    ctx.arc(C.width/2, C.width/2,20,0, 2*Math.PI,false);
+    ctx.arc(C.width/2, C.height/2,20,0, 2*Math.PI,false);
     ctx.stroke()
-    ctx.fill()
+    ctx.fill() */
+    ctx.drawImage(mario, x,0,200,200)
+    ctx.lineWidth = 1
+    ctx.fillStyle = 'black'
+    ctx.font = "50px Arial"
+    ctx.textAlign = "center"
+    ctx.fillText("week 4 lab",C.width/2 , 50)
+    ctx.strokeText("week 4 lab",C.width/2 , 50)
+    console.log ('WEAREAnimating')
+    
 }
 draw()
